ee50478a0703011ae1963b564350c99ef0087aac
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-09-06 17:44:14 +0300

    Update 7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -6,4 +6,3 @@ middle_word = word[1:-1]# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
-
