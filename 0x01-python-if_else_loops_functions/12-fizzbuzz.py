2482d7fde0f572f50af6cee882cbf35631351563
bernard <bkaraba14@gmail.com>
2021-09-07 15:07:38 +0000

    updateds

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(0, 101):
+    for i in range(1, 101):
         if i % 15 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
