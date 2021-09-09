5e4a3a8287ce462d8ba1a7e67bef23ee8be46582
bernard <bkaraba14@gmail.com>
2021-09-09 10:57:29 +0000

    patches

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
+    import hidden_4
+
     strings = dir(hidden_4)
-    for i in range(0, len(strings)):
-        if strings[i][0:2] != "__":
-            print("{}".format(strings[i]))
+    for string in strings:
+        if string[:2] != "__":
+            print(string)
