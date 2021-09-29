ca25e0154b3aabfee1133bc90c996a6adb7396cf
Bkaraba <bkaraba14@gmail.com>
2021-09-29 03:39:24 -0700

    patches

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             pass
-        print()
-        return count
+    print()
+    return count
