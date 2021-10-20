27bfa3a19ce1dbd9a11ec43dd470f1db0001546d
Bkaraba <bkaraba14@gmail.com>
2021-10-19 22:19:24 -0700

    patch

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    max = 0
+    for value in a_dictionary.values():
+        if value > max:
+            max = value
+    for k, v in a_dictionary.items():
+        if v == max:
+            return k
