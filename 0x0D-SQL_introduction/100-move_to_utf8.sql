b0f0272cff39a8f6fab079c56beb7986bf4e7a48
Bkaraba <bkaraba14@gmail.com>
2021-12-07 23:52:19 -0800

    fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,5 +3,5 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- converts hbtn_0c_0.first_table table to UTF8
 ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---converts hbtn_0c_0.first_table.name field to UTF8
+-- converts hbtn_0c_0.first_table.name field to UTF8
 ALTER TABLE hbtn_0c_0.first_table MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
