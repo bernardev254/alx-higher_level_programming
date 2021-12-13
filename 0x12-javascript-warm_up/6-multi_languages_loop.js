d19799f5abd515121de20bedab60fd606847197d
Bkaraba <bkaraba14@gmail.com>
2021-12-13 10:20:12 -0800

    fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const myArry = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 333; i++) {
+for (let i = 0; i < 3; i++) {
   console.log(myArry[i]);
 }
