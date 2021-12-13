ebfe62d12c2347c70a77e807d626535d82b669ce
Bkaraba <bkaraba14@gmail.com>
2021-12-13 10:28:46 -0800

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const args = process.argv.slice(2);
 if (isNaN(args[0]) || args[0] === undefined) {
   console.log('Not a number');
 } else {
-  console.log('My number:', Number(args[1]));
+  console.log('My number:', Number(args[0]));
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArry = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArry = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(myArry[i]);
 }
