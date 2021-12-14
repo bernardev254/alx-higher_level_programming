a0740e4f6db087f34e6b751058b3d34b667f7691
Bkaraba <bkaraba14@gmail.com>
2021-12-13 23:44:26 -0800

    fix square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,12 @@ const args = process.argv.slice(2);
 if (isNaN(args[0]) || args[0] === undefined) {
   console.log('Missing size');
 } else {
-  const a  = Number(args[0]);
+  const a = Number(args[0]);
   for (let i = 0; i < a; i++) {
+    let str = '';
     for (let y = 0; y < a; y++) {
-      console.log('X');
+      str += 'X';
     }
+    console.log(str);
   }
 }
